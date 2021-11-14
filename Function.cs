3bacbefe2c0abc68bf67c904c9c005754709e607
Iaroslav Bulin <iaroslav_bulin@epam.com>
2021-11-15 01:46:30 +0300

    refactoring

--- Function.cs
+++ Function.cs
@@ -1,9 +1,13 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
 using Amazon.S3;
+using Amazon.S3.Model;
 using Amazon.S3.Transfer;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -15,7 +19,12 @@ namespace AWSLoggerLambda
 {
     public class Function
     {
-        private readonly string _bucketName = "loggerstorage";
+        //private readonly string _bucketName = "loggerstorage";
+        private readonly string _bucketName = "stack-logstorage";
+
+        //private readonly string _queueName = "SqsLoggerQueue";
+        private readonly string _queueName = "stack-sqsloggerqueue";
+
         /// <summary>
         /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
         /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
@@ -36,9 +45,6 @@ namespace AWSLoggerLambda
         /// <returns></returns>
         public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
         {
-            //string jsonString = JsonSerializer.Serialize(evnt);
-            //Console.WriteLine(jsonString);
-
             foreach (var message in evnt.Records)
             {
                 await ProcessMessageAsync(message, context);
@@ -47,34 +53,53 @@ namespace AWSLoggerLambda
 
         private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
         {
-            //Console.WriteLine(message.Body);
-            //await Task.CompletedTask;
-
-            using (var s3client = new AmazonS3Client(Amazon.RegionEndpoint.USEast2))
-            {
+                using (var s3client = new AmazonS3Client(Amazon.RegionEndpoint.USEast2))
+                {
                 var fileName = DateTime.Now.Date.ToString("yyyy-MM-dd");
+
                 var logData = message.Body;
 
-                using (var newMemoryStream = new MemoryStream())
+                var buckets = await s3client.ListBucketsAsync();
+                var bucketExist = buckets.Buckets.Any(b => b.BucketName == _bucketName);
+                if (!bucketExist)
                 {
-                    var logDataArr = Encoding.Unicode.GetBytes(logData);
-                    await newMemoryStream.WriteAsync(logDataArr, 0, logDataArr.Length);
-
-                    var uploadRequest = new TransferUtilityUploadRequest
-                    {
-                        InputStream = newMemoryStream,
-                        Key = fileName,
-                        BucketName = _bucketName,
-                        CannedACL = S3CannedACL.PublicRead
-                    };
-
-                    var fileTransferUtility = new TransferUtility(s3client);
-                    await fileTransferUtility.UploadAsync(uploadRequest);
+                    throw new AmazonS3Exception($"bucket - {_bucketName} doesnt exist");
                 }
+                //----
+                //AmazonSQSClient sqsClient = new AmazonSQSClient();
+                //var queueUrlResp = await sqsClient.GetQueueUrlAsync(_queueName);
+                //context.Logger.LogLine($"queueUrlResp.QueueUrl = {queueUrlResp.QueueUrl}");
+                //context.Logger.LogLine($"message.ReceiptHandle = {message.ReceiptHandle}");
+                //try
+                //{
+                //    var deleteMsgResp = await sqsClient.DeleteMessageAsync(queueUrlResp.QueueUrl, message.ReceiptHandle);
 
+                //    if (deleteMsgResp.HttpStatusCode != System.Net.HttpStatusCode.OK)
+                //    {
+                //        context.Logger.LogLine($"Failed to delete message {message.ReceiptHandle} from queue {_queueName}");
+                //    }
+                //}
+                //catch (Exception e)
+                //{
+                //    context.Logger.LogLine(e.Message);
+                //}
 
-                context.Logger.LogLine($"Processed message {message.Body}");
-            }
+                //-----
+                using (var newMemoryStream = new MemoryStream())
+                    {
+                        var logDataArr = Encoding.Unicode.GetBytes(logData);
+                        await newMemoryStream.WriteAsync(logDataArr, 0, logDataArr.Length);
+
+                        var req = new PutObjectRequest()
+                        {
+                            InputStream = newMemoryStream,
+                            BucketName = _bucketName,
+                            Key = fileName
+                        };
+                        await s3client.PutObjectAsync(req);
+                    }
+                    context.Logger.LogLine($"Processed message {message.Body}");
+                }
         }
     }
 
